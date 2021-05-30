26ab42a8b834b05a90d654ae8ff38f2bb71766aa
Erick <erickbarretodasilva@outlook.com>
2021-05-30 16:20:05 -0300

    projeto da aula 2

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'tokenUAHuahuUHAUua',
+    expireIn: '1d',
+  },
+}
---
+++ src/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,14 @@
+import CreateAppointmentService from './CreateAppointmentService'
+
+test('sum two numbers', () => {
+  expect(1 + 2).toBe(3)
+})
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3)
+  })
+  it('should be able to create two appointment on the same time', () => {
+    expect(1 + 2).toBe(3)
+  })
+})
